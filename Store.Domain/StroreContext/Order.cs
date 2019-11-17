ad5842d6baa88a170b6053ab4e552e5940a2adac
Mario Oliveira <maper765@hotmail.com>
2019-11-17 09:30:16 -0300

    Inicio

---
+++ Store.Domain/StroreContext/Order.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Store.Domain.StoreContext 
+{
+    public class Order 
+    {
+        public Customer Customer { get; set; }
+        public string Number { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Status { get; set; }
+        public IList<OrderItem> Items { get; set; }
+        public IList<Delivary> Delivaries { get; set; }
+
+        // To Place An Order
+        public void Place()
+        {
+            
+        }
+    }
+}
