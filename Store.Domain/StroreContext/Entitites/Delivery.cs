ff3f8dc989e768554d7c35ba8f0c9c912d2e0954
Mario Oliveira <maper765@hotmail.com>
2019-11-17 16:21:38 -0300

    Implementando pedido

--- Store.Domain/StroreContext/Entitites/Delivery.cs
+++ Store.Domain/StroreContext/Entitites/Delivery.cs
@@ -15,5 +15,17 @@ namespace Store.Domain.StoreContext.Entities
         public DateTime CreateDate { get; private set; }
         public DateTime EstimatedDeliveryDate { get; private set; }
         public EDeliveryStatus Status { get; private set; }
+
+        public void Ship()
+        {
+            // Se a data estimada de entrega for no passado, não entregar
+            Status = EDeliveryStatus.Shipped;
+        }
+
+        public void Cancel()
+        {
+            // Se o status já estiver entregue, não pode cancelar
+            Status = EDeliveryStatus.Canceled;
+        }
     }
 }
--- Store.Domain/StroreContext/Entitites/Order.cs
+++ Store.Domain/StroreContext/Entitites/Order.cs
@@ -13,10 +13,6 @@ namespace Store.Domain.StoreContext.Entities
         public Order(Customer customer)
         {
             Customer = customer;
-            Number = Guid.NewGuid()
-                .ToString()
-                .Replace("-", "")
-                .Substring(0,8).ToUpper();
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
             _items = new List<OrderItem>();
@@ -36,15 +32,54 @@ namespace Store.Domain.StoreContext.Entities
             _items.Add(item);
         }
 
-        public void AddDelivery(Delivery delivery)
-        {
-            _delivaries.Add(delivery);
-        }
-
-        // To Place An Order
+        // Criar um pedido
         public void Place()
         {
-            
+            // Gera o número do pedido
+            Number = Guid.NewGuid()
+                .ToString()
+                .Replace("-", "")
+                .Substring(0, 8).ToUpper();
+        }
+
+        // Pagar um pedido
+        public void Pay()
+        {
+            // Para fins de estudo, assume-se que o pagamento já foi realizado...
+            Status = EOrderStatus.Paid;
+        }
+
+        // Enviar um pedido
+        public void Ship()
+        {
+            // A cada 5 produtos é uma entrega
+            var deliveries = new List<Delivery>();
+            deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
+            var count = 1;
+
+            // Quebra as entregas
+            foreach (var item in _items)
+            {
+                if (count == 5)
+                {
+                    count = 1;
+                    deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
+                }
+                count++;
+            }
+
+            // Envia todas as entregas
+            deliveries.ForEach(x => x.Ship());
+
+            // Adiciona as entregas ao pedido
+            deliveries.ForEach(x => _delivaries.Add(x));
+        }
+
+        // Cancelar um pedido
+        public void Cancel()
+        {
+            Status = EOrderStatus.Canceled;
+            _delivaries.ToList().ForEach(x => x.Cancel());
         }
     }
 }
